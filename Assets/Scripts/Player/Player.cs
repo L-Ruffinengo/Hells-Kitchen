99339797766a8fa8a331fea6e068fbe069f074af
L-Ruffinengo <lauruffinengo@gmail.com>
2024-03-01 13:50:31 -0300

    Add Player collisions

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -20,8 +20,54 @@ public class Player : MonoBehaviour
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDirection * moveSpeed * Time.deltaTime;
 
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = 0.7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,
+            playerRadius, moveDirection, moveDistance);
+
+        // Diagonal movement when colliding
+        if (!canMove)
+        {
+            // Cannot move towards moveDirection
+
+            //Attempt only x movement
+            Vector3 moveDirectionX = new Vector3(moveDirection.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, 
+                playerRadius, moveDirectionX, moveDistance);
+
+            if (canMove)
+            {
+                // Can move only on X axis
+                moveDirection = moveDirectionX;
+            }
+            else
+            {
+                // Cannot move only on the X
+
+                //Attempt only z movement
+                Vector3 moveDirectionZ = new Vector3(0, 0, moveDirection.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,
+                    playerRadius, moveDirectionZ, moveDistance);
+
+                if (canMove)
+                {
+                    // Can move only on Z axis
+                    moveDirection = moveDirectionZ;
+                }
+                else
+                {
+                    // Cannot move in any direction
+                }
+            }
+        }
+
+        if(canMove)
+        {
+            transform.position += moveDirection * moveDistance;
+        }
+        
         isWalking = moveDirection != Vector3.zero;//es lo mismo que if moveDir!=Vector.zero{isWalking = true}
 
         transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotationSpeed);
