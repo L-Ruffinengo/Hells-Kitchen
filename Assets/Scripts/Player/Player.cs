2a8df5bb2eecd483f23f589c0a854a8586dd7fb8
L-Ruffinengo <lauruffinengo@gmail.com>
2024-03-01 14:28:56 -0300

    Add clear counter and its players interactions

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour
 {
@@ -13,9 +16,23 @@ public class Player : MonoBehaviour
     private float rotationSpeed;
     [SerializeField]
     private GameInput gameInput;
+    [SerializeField]
+    private LayerMask countersLayerMask;
 
     private bool isWalking;
+    private Vector3 lastInteractDir;
     private void Update()
+    {
+        HandleMovement();
+        HandleInteractions();
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+
+    private void HandleMovement()
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
@@ -34,8 +51,8 @@ public class Player : MonoBehaviour
 
             //Attempt only x movement
             Vector3 moveDirectionX = new Vector3(moveDirection.x, 0, 0).normalized;
-            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, 
-                playerRadius, moveDirectionX, moveDistance);
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up* playerHeight,
+            playerRadius, moveDirectionX, moveDistance);
 
             if (canMove)
             {
@@ -63,18 +80,35 @@ public class Player : MonoBehaviour
             }
         }
 
-        if(canMove)
+        if (canMove)
         {
             transform.position += moveDirection * moveDistance;
         }
-        
+
         isWalking = moveDirection != Vector3.zero;//es lo mismo que if moveDir!=Vector.zero{isWalking = true}
 
         transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotationSpeed);
     }
 
-    public bool IsWalking()
+    private void HandleInteractions()
     {
-        return isWalking;
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
+        Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if(moveDirection != Vector3.zero)
+        {
+            lastInteractDir = moveDirection;
+        }
+        float interactDistance = 2f;
+       if(Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask))
+        {
+            if(raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                // Has ClearCounter 
+                clearCounter.Interact();
+            }
+        }
+
     }
 }
