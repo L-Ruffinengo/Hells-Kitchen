e6bb8bc55fa7c41fa80fd2ba19806e9f5674ca0b
L-Ruffinengo <lauruffinengo@gmail.com>
2024-03-06 12:35:19 -0300

    Add Kitchen Object parent logic

--- Assets/Scripts/Counters/ClearCounter.cs
+++ Assets/Scripts/Counters/ClearCounter.cs
@@ -18,8 +18,36 @@ public class ClearCounter : MonoBehaviour
             kitchenObjectTransform.localPosition = Vector3.zero;
             
             kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
+            kitchenObject.SetClearCounter(this);
         }
-        
+        else
+        {
+            Debug.Log(kitchenObject.GetClearCounter());
+        }
+    }
 
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return counterTopPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
     }
 }
--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -7,8 +7,34 @@ public class KitchenObject : MonoBehaviour
     [SerializeField]
     private SOKitchenObject soKitchenObject;
 
+    private ClearCounter clearCounter;
+
     public SOKitchenObject GetKitchenObjectSO() 
     {
         return soKitchenObject; 
+    }
+
+    public void SetClearCounter(ClearCounter clearCounter)
+    {
+        if(this.clearCounter != null)
+        {
+            this.clearCounter.ClearKitchenObject();
+        }
+
+        this.clearCounter = clearCounter;
+
+        if (clearCounter.HasKitchenObject())
+        {
+            Debug.LogError("Counter already has a KitchenObject!");
+        }
+        clearCounter.SetKitchenObject(this);
+
+        transform.parent = clearCounter.GetKitchenObjectFollowTransform();
+        transform.localPosition = Vector3.zero;
+    }
+
+    public ClearCounter GetClearCounter()
+    {
+        return clearCounter;
     }
 }
